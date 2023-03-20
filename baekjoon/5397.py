ef74196f48f3a24ca16f177acecccdf73d074d0d
ktykty0722 <ktykty0722@naver.com>
2023-03-20 19:25:43 +0900

    키로거

---
+++ baekjoon/5397.py
@@ -0,0 +1,18 @@
+t = int(input())
+for _ in range(t):
+    s = list(input())
+    left, right = [], []
+    for i in s:
+        if i == '<':
+            if left:
+                right.append(left.pop())
+        elif i == '>':
+            if right:
+                left.append(right.pop())
+        elif i == '-':
+            if left:
+                left.pop()
+        else:
+            left.append(i)
+    left.extend(reversed(right))
+    print(''.join(left))
