d98c95245ee9ef290cf60b1a8ddb4606a268493b
ktykty0722 <ktykty0722@naver.com>
2023-05-05 22:08:38 +0900

    가장 긴 증가하는 부분 수열 4

--- baekjoon/14002.py
+++ baekjoon/14002.py
@@ -1,17 +1,20 @@
 import sys
-from bisect import bisect_left
 
 n = int(sys.stdin.readline().rstrip())
 arr = list(map(int, sys.stdin.readline().rstrip().split()))
+dp = [1 for _ in range(n)]
 
-lis = [arr[0]]
+for i in range(n):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
 
-for a in arr:
-    if lis[-1] < a:
-        lis.append(a)
-    else:
-        idx = bisect_left(lis, a)
-        lis[idx] = a
-
-print(len(lis))
-print(*lis)
+print(max(dp))
+x = max(dp)
+ans = []
+for i in reversed(range(n)):
+    if dp[i] == x:
+        ans.append(arr[i])
+        x -= 1
+ans.reverse()
+print(*ans)
